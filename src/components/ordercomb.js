c0f97653f3832d9e8ef06d0aff9d43d9b00c75a2
WhitneyD <whitney.dubourg@outlook.com>
2019-09-23 00:59:53 -0500

    updates to router paths

--- src/components/ordercomb.js
+++ src/components/ordercomb.js
@@ -47,7 +47,7 @@ export default class Ordercomb extends Component {
 
 				</svg>
 
-				<NavLink to="/honey" className="nav-link" activeClassName="selected">order</NavLink>
+				<NavLink to="/order" className="nav-link" activeClassName="selected">order</NavLink>
 				
 			</div>
 		)
