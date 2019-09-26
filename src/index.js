107c6243043e099cfe507ba42f1c9769a0d9bac9
WhitneyD <whitney.dubourg@outlook.com>
2019-09-26 00:19:18 -0500

    added web.config file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './web.config.xml';
 
 var domContainer = document.querySelector('#content-container');
 
