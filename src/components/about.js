0c0f9f3d2dc2b904a29dc09d9abb19e3041ab693
WhitneyD <whitney.dubourg@outlook.com>
2019-10-01 18:16:29 -0500

    updates to css

--- src/components/about.js
+++ src/components/about.js
@@ -22,7 +22,7 @@ export default class About extends Component {
 
 		<div className= "about-wrapper" ref={this.scrollIntoFocus}>
 
-			<figure className="about-figure">
+			<figure className="aboutFigure">
 
 				<img className="about-photo" src={ require ('./img/keepers.jpg') } alt="bee keepers" />
 
@@ -31,7 +31,7 @@ export default class About extends Component {
 			<p className="about-text"><span className="title-first">NOLA</span> <span className="title-second">honey</span> was founded in 2018. We are locally owned and operated, organic raw honey producers. 
 			We believe in  doing things nature’s way without the use of chemicals.</p>
 
-			<figure className="about-figure">
+			<figure className="aboutFigure">
 
 				<img className="about-photo" src={ require ('./img/holding.jpg') } alt="bee keeper holding a beehive frame" />
 
@@ -40,7 +40,7 @@ export default class About extends Component {
 			<p className="about-text"><span className="title-first">OUR</span> <span className="title-second">mission</span> is to provide the Greater New Orleans Area with the pure, sweet taste of local honey. 
 			Our honey is as good for your health as it is to your tummy. <img src={ require ('./img/yummy.png') }  alt="yum emoji"/></p>
 
-			<figure className="about-figure bees">
+			<figure className="aboutFigure bees">
 
 				<img className="about-photo" src={ require ('./img/bees.jpg') } alt="close up of bees on a frame" />
 		
