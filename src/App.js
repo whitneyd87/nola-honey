d4373d7dfc361e41e5c09e8c1a58a4869b5ae7dc
WhitneyD <whitney.dubourg@outlook.com>
2020-02-17 11:54:37 -0600

    updated favicon import

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import Hive from './components/hive.js';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faCoffee, faTshirt, faInfo, faAt, faTint, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab, faFacebookF, faInstagram, faTwitter, fas, faCoffee, faTshirt, faAngleDown, faAt, faInfo, faTint);
+library.add(fab, fas);
 
 class App extends React.Component {  	
 
